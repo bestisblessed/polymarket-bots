a65363c31fe62e27ec3324620ff22055f005ca25
bestisblessed <tyler.durette@gmail.com>
2025-12-10 05:14:01 -0500

    im dumb

--- my-openai-whale-bot/run.sh
+++ my-openai-whale-bot/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# */3 * * * * /home/trinity/polymarket-bots/my-openai-whale-bot/run.sh >> /home/trinity/polymarket-bots/my-openai-whale-bot/log_whales.log 2>&1
+# 2-59/4 * * * * /home/trinity/polymarket-bots/my-openai-whale-bot/run.sh >> /home/trinity/polymarket-bots/my-openai-whale-bot/log_whales.log 2>&1
 
 export PATH="$HOME/.pyenv/shims:$HOME/.pyenv/bin:$PATH"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
