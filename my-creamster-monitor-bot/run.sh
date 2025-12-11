00f4827cd431c1745b0e2d92c34c58cce1ad6f42
bestisblessed <tyler.durette@gmail.com>
2025-12-11 14:22:00 -0500

    im dumb

--- my-creamster-monitor-bot/run.sh
+++ my-creamster-monitor-bot/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Cron: 6-59/8 * * * * /home/trinity/polymarket-bots/my-creamster-monitor-bot/run.sh
+# Cron: 6-59/8 * * * * /home/trinity/polymarket-bots/my-creamster-monitor-bot/run.sh >> /home/trinity/polymarket-bots/my-creamster-monitor-bot/log.log 2>&1
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
