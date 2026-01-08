6e797099884c1fe7adbc7a107f5c112976ee133a
bestisblessed <tyler.durette@gmail.com>
2026-01-08 14:30:15 -0500

    im dumb

--- my-sports-bot-bak/run_monitor_game_holders.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# */3 * * * * /home/trinity/polymarket-bots/my-sports-bot/run_monitor_game_holders.sh >> /home/trinity/polymarket-bots/my-sports-bot/log_whales.log 2>&1
-
-export PATH="$HOME/.pyenv/shims:$HOME/.pyenv/bin:$PATH"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR"
-python monitor_game_holders.py
