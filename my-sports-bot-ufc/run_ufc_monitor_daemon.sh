a81f19fab2eb4b3ead44e2e78bc41408d1643320
Trinity <tyler.durette@gmail.com>
2026-01-09 13:05:55 -0500

    im dumb

--- my-sports-bot-ufc/run_ufc_monitor_daemon.sh
+++ my-sports-bot-ufc/run_ufc_monitor_daemon.sh
@@ -20,7 +20,7 @@
 
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")}" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
 # Session name for screen/tmux
